23592886427d21807aac3fa03e8636a0c117aa0e
周睿 <zrufo747@outlook.com>
2024-10-29 05:43:23 +0000

    windows path

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,11 @@ fn main() -> Result<()> {
     workdir = fs::canonicalize(&workdir)
         .unwrap_or_else(|_| panic!("Workdir not found: {}", workdir.display()));
 
+    #[cfg(target_os = "windows")]
+    {
+        workdir = PathBuf::from(format!("{}", workdir.display()).trim_start_matches("\\\\?\\"));
+    }
+
     println!("Workdir: {}", workdir.display());
 
     let mut project = Project::new(workdir, cli.config)?;
