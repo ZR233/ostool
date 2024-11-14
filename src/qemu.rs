a4a7bf9fd33f6c3b7c8417e2ee544b6bf07281b2
周睿 <zrufo747@outlook.com>
2024-11-14 14:28:13 +0800

    windows find qemu

--- src/qemu.rs
+++ src/qemu.rs
@@ -1,6 +1,9 @@
 use std::{
+    collections::HashMap,
+    ffi::{OsStr, OsString},
     fs,
-    process::exit,
+    path::PathBuf,
+    process::{exit, Command},
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
@@ -30,6 +33,10 @@ impl Qemu {
         let bin_path = fs::canonicalize(bin_path).unwrap();
 
         let mut cmd = project.shell(project.arch.unwrap().qemu_program());
+
+        #[cfg(target_os = "windows")]
+        Self::cmd_windows_env(&mut cmd);
+
         if !project.config_ref().qemu.graphic {
             cmd.arg("-nographic");
         }
@@ -76,5 +83,36 @@ impl Qemu {
         } else {
             cmd.exec(project.is_print_cmd).unwrap();
         }
+    }
+
+    fn cmd_windows_env(cmd: &mut Command) {
+        let env = cmd.get_envs().collect::<HashMap<_, _>>();
+        let mut mysys2_root = PathBuf::from("C:\\msys64");
+        if let Some(p) = std::env::var_os("MSYS2_ROOT") {
+            mysys2_root = PathBuf::from(p);
+        }
+
+        let mut path = env
+            .get(OsStr::new("PATH"))
+            .unwrap_or(&None)
+            .unwrap_or_default()
+            .to_os_string();
+        if !path.is_empty() {
+            path.push(";");
+        }
+
+        let ucrt64 = mysys2_root.join("ucrt64/bin");
+
+        if ucrt64.join("qemu-system-x86_64.exe").exists() {
+            path.push(OsString::from(ucrt64));
+        }
+
+        let mingw64 = mysys2_root.join("mingw64/bin");
+
+        if mingw64.join("qemu-system-x86_64.exe").exists() {
+            path.push(mingw64);
+        }
+
+        cmd.env("PATH", path);
     }
 }
