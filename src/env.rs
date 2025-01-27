a4ee451c40a8d1fc34ae3188c9d85e678adc42fd
dbydd <1992003927@qq.com>
2025-01-27 15:39:21 +0800

    make MSYS2 optinal

--- src/env.rs
+++ src/env.rs
@@ -9,34 +9,36 @@ pub fn prepere_deps() {
 
 #[cfg(target_os = "windows")]
 fn windows() {
+    
+
     use colored::Colorize;
     let mut mysys2_root = PathBuf::from("C:\\msys64");
     if let Some(p) = std::env::var_os("MSYS2_ROOT") {
         mysys2_root = PathBuf::from(p);
     }
 
-    if !mysys2_root.exists() {
-        println!("{}", "MSYS2 not found!".yellow());
-        return;
-    }
+    if mysys2_root.exists() {
+        println!("{}", "MSYS2 found!".green());
 
-    println!("{}", "MSYS2 found!".green());
+        let ucrt64 = mysys2_root.join("ucrt64/bin");
+        let mingw64 = mysys2_root.join("mingw64/bin");
+        let usr_bin = mysys2_root.join("usr/bin");
 
-    let ucrt64 = mysys2_root.join("ucrt64/bin");
-    let mingw64 = mysys2_root.join("mingw64/bin");
-    let usr_bin = mysys2_root.join("usr/bin");
+        let mut path = vec![usr_bin];
 
-    let mut path = vec![usr_bin];
+        if ucrt64.join("qemu-system-x86_64.exe").exists() {
+            path.push(ucrt64);
+        } else if mingw64.join("qemu-system-x86_64.exe").exists() {
+            path.push(mingw64);
+        } else {
+            println!("{}", "QEMU not found!".yellow());
+        }
 
-    if ucrt64.join("qemu-system-x86_64.exe").exists() {
-        path.push(ucrt64);
-    } else if mingw64.join("qemu-system-x86_64.exe").exists() {
-        path.push(mingw64);
+        let _ = EXTRA_PATH.set(path);
     } else {
-        println!("{}", "QEMU not found!".yellow());
+        println!("{}", "MSYS2 not found! try to use local apps".yellow());
+        let _ = EXTRA_PATH.set(Vec::new());
     }
-
-    let _ = EXTRA_PATH.set(path);
 }
 
 pub fn get_extra_path() -> Vec<PathBuf> {
